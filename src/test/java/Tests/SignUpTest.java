7c9875826a7cfde9bbb07b4ce62957d5c79307a3
Dr.Gonzo <Gonzo@yahoo.com>
2023-07-03 13:59:22 -0700

    Sign up page test revised

--- src/test/java/Tests/SignUpTest.java
+++ src/test/java/Tests/SignUpTest.java
@@ -1,34 +1,44 @@
 package Tests;
 
-import Pages.LoginPage;
 import Pages.SignUpPage;
-import Utils.ConfigReader;
 import Utils.Driver;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class SignUpTest extends TestBase {
-@Test
-   public void SignUp(){
+    @Test
+    public void SignUp() {
 
-       SignUpPage signup = new SignUpPage();
+        SignUpPage signup = new SignUpPage();
 
-       String firstName = signup.getRandomFirstName();
-       String lastName = signup.getRandomLastName();
-       String email = signup.getRandomEmail();
-       String PassWord =  signup.getRandomPassword();
+        String firstName = signup.getRandomFirstName();
+        String lastName = signup.getRandomLastName();
+        String email = signup.getRandomEmail();
+        String PassWord = signup.getRandomPassword();
 
-      signup.getSignUpButton().click();
-      signup.getFirstName().sendKeys(firstName);
-      signup.getLastName().sendKeys(lastName);
-      signup.getEmail().sendKeys(email);
-      signup.getPassword1().sendKeys(PassWord);
-      signup.getRegisterButton().click();
-      Assert.assertEquals(Driver.getDriver().getTitle(), "Register - Create an Account");
+        signup.getSignUpButton().click();
+        signup.getFirstName().sendKeys(firstName);
+        signup.getLastName().sendKeys(lastName);
+        signup.getEmail().sendKeys(email);
+        signup.getPassword1().sendKeys(PassWord);
+        signup.getRegisterButton().click();
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Register - Create an Account");
 
-   }
+        writeCredentialsToCSV(email, PassWord);
+    }
 
+    public void writeCredentialsToCSV(String email, String PassWord) {
+        String filePath = "SignUp.csv";
+
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            writer.append(email).append(",").append(PassWord).append("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
 
