9f2e0b1c263186b3b6ff1b5d46506624a2b790ac
Dr.Gonzo <Gonzo@yahoo.com>
2023-07-03 14:56:47 -0700

    Sign up page test revised

---
+++ src/test/java/Tests/SignupWrongEmail.java
@@ -0,0 +1,41 @@
+package Tests;
+
+import Pages.SignUpPage;
+import Utils.Driver;
+import org.testng.Assert;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SignupWrongEmail extends TestBase {
+    public void SignupWrongEmail(){
+
+        SignUpPage signup = new SignUpPage();
+
+        String firstName = signup.getRandomFirstName();
+        String lastName = signup.getRandomLastName();
+        String email = signup.getRandomEmail();
+        String PassWord = signup.getRandomPassword();
+
+        signup.getSignUpButton().click();
+        signup.getFirstName().sendKeys(firstName);
+        signup.getLastName().sendKeys(lastName);
+        signup.getEmail().sendKeys(email);
+        signup.getPassword1().sendKeys(PassWord);
+        signup.getRegisterButton().click();
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Register - Create an Account");
+
+        writeCredentialsToCSV(email, PassWord);
+    }
+
+    public void writeCredentialsToCSV(String email, String PassWord) {
+        String filePath = "SignUp.csv";
+
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            writer.append(email).append(",").append(PassWord).append("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
