3519bc783a2781bf9cbd48d717d624db378bcf31
zinam <zingerny@gmail.com>
2023-07-09 00:26:49 -0400

    "Zina-verify welcome mesg"

--- src/test/java/Tests/WelcomeMessage.java
+++ src/test/java/Tests/WelcomeMessage.java
@@ -3,13 +3,15 @@ package Tests;
 import Utils.Driver;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class WelcomeMessage extends TestBase{
+    @Test
 
     public void verifyMessage(){
        String actualMessage = Driver.getDriver().findElement(By.xpath("//div[@class='card-title']//h4[@class='text-center mb-2']")).getText();
         Assert.assertEquals(actualMessage,"Welcome Back!");
-
+        System.out.println(actualMessage);
     }
 
 
