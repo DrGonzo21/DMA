9520d837a734b08d409034cb64628c362dc9e695
Dr.Gonzo <Gonzo@yahoo.com>
2023-07-10 10:01:42 -0700

    new email error test

--- src/test/java/Tests/SignupDupEmail.java
+++ src/test/java/Tests/SignupDupEmail.java
@@ -1,6 +1,8 @@
 package Tests;
 
 import Pages.SignUpPage;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
@@ -11,44 +13,18 @@ import java.io.IOException;
 public class SignupDupEmail extends TestBase {
 
     @Test
-    public void SignupDupEmail() {
-        String filePath = "SignUp.csv";
+    public void SignupDupEmail() throws InterruptedException {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] record = line.split(",");
-                if (record.length == 2) {
-                    String email = record[0].trim();
 
                     SignUpPage signup = new SignUpPage();
 
-                    String firstName = signup.getRandomFirstName();
-                    String lastName = signup.getRandomLastName();
-                    String password = signup.getRandomPassword();
-
                     signup.getSignUpButton().click();
-                    signup.getFirstName().sendKeys(firstName);
-                    signup.getLastName().sendKeys(lastName);
-                    signup.getEmail().sendKeys(email);
-                    signup.getPassword1().sendKeys(password);
-                    signup.getRegisterButton().click();
-
-                    writeCredentialsToCSV(email, password);
+                    signup.getFirstName().sendKeys("John");
+                    signup.getLastName().sendKeys("does");
+                    signup.getEmail().sendKeys("123123@example.com", Keys.TAB);
+                    Thread.sleep(2000);
+                     Assert.assertTrue(signup.getEmailerror().isDisplayed());
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public void writeCredentialsToCSV(String email, String password) {
-        String filePath = "SignupDuplicate.csv";
 
-        try (FileWriter writer = new FileWriter(filePath, true)) {
-            writer.append(email).append(",").append(password).append("\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
