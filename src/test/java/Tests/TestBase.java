f943ff30af5437d63e76d2ee529fec59b0b8944c
Dr.Gonzo <Gonzo@yahoo.com>
2023-07-12 12:35:01 -0700

    new TestBase

--- src/test/java/Tests/TestBase.java
+++ src/test/java/Tests/TestBase.java
@@ -1,25 +1,70 @@
 package Tests;
-
 import Utils.ConfigReader;
 import Utils.Driver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.BeforeSuite;
+import Utils.SeleniumWebTools;
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 public class TestBase {
-    @BeforeMethod(alwaysRun = true)
-    public void setUp(){
 
-        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    protected static ExtentReports report; // manages the report generation
+    protected static ExtentSparkReporter htmlReport; // creates the html report
+    protected static ExtentTest logger; // logger object
+
+    @BeforeSuite(alwaysRun = true)
+    public void setupReport(){
+        report =  new ExtentReports();
+        String pathToHtmlFile = System.getProperty("user.dir")+"/target/ExtentReports/report.html";
+        htmlReport = new ExtentSparkReporter(pathToHtmlFile);
+        report.attachReporter(htmlReport);
+        report.setSystemInfo("Project Name", "Web Orders Automation Tests");
+        report.setSystemInfo("SDET", "Team A");
+        report.setSystemInfo("Environment", ConfigReader.getProperty("env"));
+        report.setSystemInfo("Operating System", System.getProperty("os.name"));
+        report.setSystemInfo("Browser", ConfigReader.getProperty("browser"));
+        report.setSystemInfo("Homepage", ConfigReader.getProperty("url"));
+    }
+
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method){
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get(ConfigReader.getProperty("url"));
+        logger = report.createTest(method.getName());
+        logger.info("TEST STARTED: " + method.getName());
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(){
+    public void tearDown(ITestResult testResult){
+
+        if (testResult.getStatus() == ITestResult.SUCCESS){
+            logger.pass("TEST PASSED: " + testResult.getName());
+        } else if (testResult.getStatus() == ITestResult.FAILURE) {
+            logger.fail("TEST FAILED: " + testResult.getName());
+            logger.fail(testResult.getThrowable());
+            String pathToScreenshotFile = SeleniumWebTools.getScreenshot("failed");
+            logger.addScreenCaptureFromPath(pathToScreenshotFile);
+        }else{
+            logger.skip("TEST SKIPPED: " + testResult.getName());
+        }
 
         Driver.quitDriver();
+    }
+
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDownReport(){
+        report.flush();
     }
 }
 
