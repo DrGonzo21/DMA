0a64d0247b92fbc94e29cdb75aa626b57df710ca
zinam <zingerny@gmail.com>
2023-07-13 21:24:53 -0400

    "DMA38 US3, verify ogo display test"

--- src/test/java/Pages/DashboardPage.java
+++ src/test/java/Pages/DashboardPage.java
@@ -1,2 +1,39 @@
-package Pages;public class DashboardPage {
+package Pages;
+
+import Utils.ConfigReader;
+import Utils.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+
+    public   DashboardPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+        LoginPage login = new LoginPage();
+        login.getEmailAddress().sendKeys(ConfigReader.getProperty("valid")  );
+        login.getPassword().sendKeys(ConfigReader.getProperty("passcode"));
+        login.getSignInButton().click();
+        login.getMortgageApp().click();
+            }
+
+
+
+    @FindBy(xpath = "//a[@href='mortgage.php']")
+    private WebElement mortgageApplication;
+    @FindBy(xpath = "//div[@class='brand-logo']")
+    private WebElement bankLogo;
+
+
+
+    public WebElement getBankLogo() {
+        return bankLogo;
+    }
+
+    public WebElement getMortgageApplication() {
+        return mortgageApplication;
+    }
 }
+
+
--- src/test/java/Tests/DMA38_US3_VerifyLogoDisplayed.java
+++ src/test/java/Tests/DMA38_US3_VerifyLogoDisplayed.java
@@ -1,2 +1,15 @@
-package Tests;public class DMA38_US3_VerifyLogoDisplayed {
+package Tests;
+
+import Pages.DashboardPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DMA38_US3_VerifyLogoDisplayed extends TestBase{
+@Test
+    public void verifyLogoDisplayed(){
+
+        DashboardPage getLogo = new DashboardPage();
+        boolean isLogoDisplayed = getLogo.getBankLogo().isDisplayed();
+        Assert.assertTrue(isLogoDisplayed, "Bank Logo is NOT displayed");
+    }
 }
