b7b4a88a0d70480db7e7c68d0cfa014617970892
zinam <zingerny@gmail.com>
2023-07-14 01:08:16 -0400

    "DMA37 US3,  verify Dashboard, trying to catch screenshot bug"

--- src/test/java/Tests/DMA38_US3_VerifyLogoDisplayed.java
+++ src/test/java/Tests/DMA38_US3_VerifyLogoDisplayed.java
@@ -1,15 +1,33 @@
 package Tests;
 
 import Pages.DashboardPage;
+import org.openqa.selenium.Point;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DMA38_US3_VerifyLogoDisplayed extends TestBase{
+
 @Test
     public void verifyLogoDisplayed(){
 
         DashboardPage getLogo = new DashboardPage();
-        boolean isLogoDisplayed = getLogo.getBankLogo().isDisplayed();
+        boolean isLogoDisplayed = getLogo.getBankIcon().isDisplayed();
         Assert.assertTrue(isLogoDisplayed, "Bank Logo is NOT displayed");
     }
+@Test
+    public void verifyLogoLocation(){
+    DashboardPage getLogo = new DashboardPage();
+    Point logoLocation = getLogo.getBankIcon().getLocation();
+    int Y = logoLocation.getY();
+    int X = logoLocation.getX();
+    System.out.println("Logo Located: " + logoLocation);
+
+    if ( Y < 50 & X < 50){
+        Assert.assertTrue(true, " Logo is NOT located in the top left corner");
+    }
+    }
+
+
+
 }
+
