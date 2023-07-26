a80f5f7620f4f966bc6460b3f5f1ee5c087ffc60
zinam <zingerny@gmail.com>
2023-07-25 23:50:03 -0400

    " DMA15 FIXED!"

--- src/test/java/Tests/DMA15_VerifySignInButtonDisabled.java
+++ src/test/java/Tests/DMA15_VerifySignInButtonDisabled.java
@@ -3,13 +3,14 @@ package Tests;
 import Pages.LoginPage;
 import Utils.ConfigReader;
 import Utils.Driver;
-import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class DMA15_VerifySignInButtonDisabled extends TestBase{
 
@@ -18,13 +19,13 @@ public class DMA15_VerifySignInButtonDisabled extends TestBase{
 
         LoginPage emailLogInOnly = new LoginPage();
 
-//          emailLogInOnly.getEmailAddress().sendKeys("Jsmith@yahoo.com");
-//
-//        Thread.sleep(500);
-//        emailLogInOnly.getSignInButton().click();
-//        boolean isSignInButtonDisabled = emailLogInOnly.isButtonDisabledSignInWithEmailOnly(emailLogInOnly.getSignInButton());
-//        Assert.assertTrue(isSignInButtonDisabled, "Button is Disabled");
-//
-//        Driver.quitDriver();
+          emailLogInOnly.getEmailAddress().sendKeys("Jsmith@yahoo.com");
+
+        Thread.sleep(500);
+        emailLogInOnly.getSignInButton().click();
+        boolean isSignInButtonDisabled = emailLogInOnly.isButtonDisabledSignInWithEmailOnly(emailLogInOnly.getSignInButton());
+        Assert.assertTrue(isSignInButtonDisabled, "Button is Disabled");
+
+        Driver.quitDriver();
     }
 }
