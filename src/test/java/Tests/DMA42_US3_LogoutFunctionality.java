28085048d5a155ae8cfe00c5f6b53316d1903566
zinam <zingerny@gmail.com>
2023-07-25 23:47:26 -0400

    " DMA42 extra test"

--- src/test/java/Tests/DMA42_US3_LogoutFunctionality.java
+++ src/test/java/Tests/DMA42_US3_LogoutFunctionality.java
@@ -7,13 +7,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DMA42_US3_LogoutFunctionality extends TestBase {
-@Test
-    public void verifyLogoutBtn() throws InterruptedException {
+
+@Test (groups = {"smoke"})
+public void verifyLogoutBtn() throws InterruptedException {
         DashboardPage logoutButton = new DashboardPage();
         logoutButton.getAccountInfo().click();
         Thread.sleep(500);
         logoutButton.getLogout().click();
     Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigReader.getProperty("url"),"You did not logout");
     }
+
+    @Test
+    public void dropdownMenuDisplayed() throws InterruptedException {
+    DashboardPage dropdown = new DashboardPage();
+    dropdown.getAccountInfo().click();
+    Thread.sleep(500);
+    Assert.assertTrue(dropdown.getLogout().isDisplayed());
+    }
+
 
 }
